f53a17aedd6ea7684dda0953586e52b9c175060f
Lego Haryanto <lego.haryanto@oracle.com>
2025-12-05 22:26:23 -0800

    Revision of Day 6 - cleanup

--- day-06.py
+++ day-06.py
@@ -14,10 +14,8 @@ arr = [list(map(int, x.split())) for x in orig_arr]
 arr = list(zip(*arr))  # flip the array (mirror based on main diagonal)
 print(sum(calc(x, y) for x, y in zip(arr, ops)))
 
-arr = list(zip(*orig_arr))[::-1]                      # easier to transpose the matrix counter clockwise
-arr = list(map(lambda x: ''.join(x).strip(), arr))    # then combine into strings, each calculation will be
-
-# group the subarrays delimited by empty string element
-arr = [list(group) for key, group in groupby(arr, key=bool) if key]
-arr = [list(map(int, x)) for x in arr]
+arr = list(zip(*orig_arr))[::-1]                                      # easier to transpose the matrix counter clockwise
+arr = list(map(lambda x: ''.join(x).strip(), arr))                    # then combine into strings
+arr = [list(group) for key, group in groupby(arr, key=bool) if key]   # unflatten the array into 2D, delimited by empty string
+arr = [list(map(int, x)) for x in arr]                                # convert all strings to integers
 print(sum(calc(x, y) for x, y in zip(arr, ops[::-1])))
