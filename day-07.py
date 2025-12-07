51f1cc17d1255334cdb62cdf8d5b6030eb33cff3
Lego Haryanto <lego.haryanto@oracle.com>
2025-12-06 23:16:17 -0800

    Revised Day 7

--- day-07.py
+++ day-07.py
@@ -1,30 +1,21 @@
 from functools import cache
+from collections import defaultdict
 
 grid = [list(x.rstrip()) for x in open(0)]
 R, C = len(grid), len(grid[0])
+cc = {grid[0].index('S'): 1}
 
-def part1(cs):
-    res = 0
-    for r in range(1, R-1):
-        next_cs = set()
-        for c in cs:
-            if grid[r+1][c] == '^':
-                res += 1
-                next_cs.add(c-1)
-                next_cs.add(c+1)
-            else:
-                next_cs.add(c)
-        cs = next_cs
-    return res
+splits = 0
+for r in range(1, R-1):
+    next_cc = defaultdict(int)
+    for c, w in cc.items():
+        if grid[r+1][c] == '^':
+            splits += 1
+            next_cc[c-1] += w
+            next_cc[c+1] += w
+        else:
+            next_cc[c] += w
+    cc = next_cc
 
-@cache
-def part2(r, c):
-    if r == R-1: return 1
-    if grid[r+1][c] == '^':
-        return part2(r+1, c-1) + part2(r+1, c+1)
-    else:
-        return part2(r+1, c)
-
-sc = grid[0].index('S')
-print(part1({sc}))
-print(part2(1, sc))
+print(splits)
+print(sum(cc.values()))
