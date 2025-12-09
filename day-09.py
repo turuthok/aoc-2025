a4c2ec9638de5473856c579df9ff7655d45d58e6
Lego Haryanto <lego.haryanto@oracle.com>
2025-12-09 09:32:30 -0800

    Day 9 - added prefix sum to speed up

--- day-09.py
+++ day-09.py
@@ -11,7 +11,7 @@ def calc(p1, p2):
 
 print(max(calc(p[i], p[j]) for i in range(n) for j in range(i+1, n)))
 
-xset = {-1, 1_000_000} # make a coordinate that's extreme min and max
+xset = {-1, 1_000_000}
 yset = {-1, 1_000_000}
 for pp in p:
     xset.add(pp[0])
@@ -24,18 +24,18 @@ def compress(s):
         l.append(value)
     return l, d
 
+# Do coordinate compression to have smaller domain
 px, xx = compress(xset)
 py, yy = compress(yset)
-R, C = len(py), len(px)
+p = list(map(lambda pp: (xx[pp[0]], yy[pp[1]]), p))
 
+# Create a compressed grid
+R, C = len(py), len(px)
 grid = [['.'] * C for _ in range(R)]
 
 p.append(p[0])
 
-for i, pp in enumerate(p):
-    x, y = pp
-    p[i] = xx[x], yy[y]
-
+# Draw the polygon with '#' as the boundary character.
 for i in range(n):
     x1, y1 = p[i]
     x2, y2 = p[i+1]
@@ -50,6 +50,7 @@ for i in range(n):
         grid[y1][x1] = '#'
         x1 += dx; y1 += dy
 
+# Flood fill using BFS, to mark the outside region with 'x'
 q = [(0, 0)]
 for r, c in q:
     for rr, cc in [(r+1, c), (r-1, c), (r, c-1), (r, c+1)]:
@@ -57,12 +58,16 @@ for r, c in q:
             grid[rr][cc] = 'x'
             q.append((rr, cc))
 
+prefix_sum = [[0] * C for _ in range(R)]
+for r in range(R):
+    for c in range(1, C):
+        prefix_sum[r][c] = prefix_sum[r][c-1] + (1 if grid[r][c] == 'x' else 0)
+
 def valid(p1, p2):
     x1, x2 = min(p1[0], p2[0]), max(p1[0], p2[0])
     y1, y2 = min(p1[1], p2[1]), max(p1[1], p2[1])
     for y in range(y1, y2+1):
-        for x in range(x1, x2+1):
-            if grid[y][x] == 'x': return False
+        if prefix_sum[y][x2] - prefix_sum[y][x1-1] > 0: return False
     return True
 
 def orig(pp):
